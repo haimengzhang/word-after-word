12390f1b11ae00f314e35ceca266d67f68c6fd9a
Haimeng Zhang <hzhang1@macalester.edu>
2019-04-13 15:53:01 -0500

    adding front page img

--- writing-app/src/App.js
+++ writing-app/src/App.js
@@ -1,28 +1,21 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.png'
+import './App.css'
 
 class App extends Component {
-  render() {
+  render () {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <p>Start writing today.</p>
         </header>
+        <body>
+          <p>Write a short story where the main characte is a "House".</p>
+        </body>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
