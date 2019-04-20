00dbe3285ea6fa575bf6ad142abb7e07650c44b5
haimengzhang <hzhang1@macalester.edu>
2019-04-19 23:55:39 -0500

    Shuffle now works

--- writing-app/src/App.js
+++ writing-app/src/App.js
@@ -1,16 +1,56 @@
 import React, { Component } from 'react'
 import logo from './logo.png'
 import './App.css'
-import Prompt from './components/Prompt'
+// import Prompt from './components/Prompt'
+import {PROMPTS} from './shared/prompts.js'
 
+
+
+class Prompt extends Component {
+
+  render () {
+    return (
+      <div className='typing-prompt'>
+        <p> {this.props.value.description}</p>
+        
+      </div>
+    )
+  }
+}
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state={
+      prompts: PROMPTS,
+      curIndex: 0,
+    }
+  }
+  renderPrompt(curIndex){
+    return (
+      <Prompt value={this.state.prompts[curIndex]}></Prompt>
+    )
+  }
+  shuffle(curIndex){
+    if (curIndex + 1 < PROMPTS.length){
+      this.setState({
+        curIndex: this.state.curIndex + 1
+      })
+    } else {
+      this.setState({curIndex:0,})
+    }
+
+  }
   render () {
     return (
       <div className='App'>
         <header className='App-header'>
           <img src={logo} className='App-logo' alt='logo' />
           <h4> Start writing today.</h4>
-          <Prompt />
+          <div>
+          {this.renderPrompt(this.state.curIndex)}
+          </div>
+          <button className="shuffle" onClick={ () => this.shuffle(this.state.curIndex)}> Shuffle </button>
+
         </header>
         <body />
       </div>
