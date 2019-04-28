d511825189686a7bfc5c089f57f32a14bab3b499
Fan Zhang <vanadiumzhang@gmail.com>
2019-04-27 20:48:59 -0500

    CHANGE: move the prompt class and methods of prompt to Prompt.js

--- writing-app/src/App.js
+++ writing-app/src/App.js
@@ -1,58 +1,23 @@
 import React, { Component } from 'react'
 import logo from './logo.png'
 import './App.css'
-// import Prompt from './components/Prompt'
-import {PROMPTS} from './shared/prompts.js'
+import Prompt from './components/Prompt'
 
-
-
-class Prompt extends Component {
-
-  render () {
-    return (
-      <div className='typing-prompt'>
-        <p> {this.props.value.description}</p>
-        
-      </div>
-    )
-  }
-}
 class App extends Component {
   constructor(props){
-    super(props)
-    this.state={
-      prompts: PROMPTS,
-      curIndex: 0,
-    }
-  }
-  renderPrompt(curIndex){
-    return (
-      <Prompt value={this.state.prompts[curIndex]}></Prompt>
-    )
-  }
-  shuffle(curIndex){
-    if (curIndex + 1 < PROMPTS.length){
-      this.setState({
-        curIndex: this.state.curIndex + 1
-      })
-    } else {
-      this.setState({curIndex:0,})
-    }
+    super(props);
+    this.state = {
+    };
+  };
 
-  }
   render () {
     return (
       <div className='App'>
         <header className='App-header'>
           <img src={logo} className='App-logo' alt='logo' />
           <h4> Start writing today.</h4>
-          <div>
-          {this.renderPrompt(this.state.curIndex)}
-          </div>
-          <button className="shuffle" onClick={ () => this.shuffle(this.state.curIndex)}> Shuffle </button>
-
         </header>
-        <body />
+        <Prompt/>
       </div>
     )
   }
